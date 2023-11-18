9a60be011e48038294f8a586f5144b89b7056082
2005Mohammadali <143836071+2005Mohammadali@users.noreply.github.com>
2023-11-18 08:39:37 +0530

    Update for_loops.js

--- for_loops.js
+++ for_loops.js
@@ -115,4 +115,19 @@ for(let i = 1; i <= n; i++){
 }
 console.log(fact)
 
+//Task - print prime numbers
+let n = Number.parseInt(prompt("Enter any number"))
+for(let i = 2; i <= n; i++){
+    let isPrime = 1
+    for(let j = 2; j < i; j++){
+        if(i % j == 0){
+            isPrime = 0
+            break
+        }
+    }
+    if(isPrime == 1){
+        console.log(i)
+    }
+}
+
 
